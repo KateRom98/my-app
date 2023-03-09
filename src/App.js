4dbb086125b052c6b012fb347590751974d3d931
KateRom98 <106525538+KateRom98@users.noreply.github.com>
2023-03-09 22:51:09 +0100

    Created a core for a weather react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WeatherSearch from "./WeatherSearch";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather forecast🌡</h1>
+      <WeatherSearch />
+      <footer>
+        <a href="" target="_blank">
+          Open-source code
         </a>
-      </header>
+        by Kateryna Dibtsova
+      </footer>
     </div>
   );
 }
-
-export default App;
